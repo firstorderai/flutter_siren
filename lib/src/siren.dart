6bce22518d307f3497669a1b745a9c79e759423e
kenzo-ai <kenzo@firstorder.ai>
2025-09-12 17:33:00 +0800

    add promptUpdateV2

--- lib/src/siren.dart
+++ lib/src/siren.dart
@@ -12,8 +12,7 @@ import './services/siren_google_play_store.dart';
 
 /// This class allows you to check the application store for new versions of your app.
 class Siren {
-  SirenStoreResponse _response =
-      SirenStoreResponse(version: '', package: '', url: '');
+  SirenStoreResponse _response = SirenStoreResponse(version: '', package: '', url: '');
 
   static SirenStoreService _getStoreClient() {
     if (Platform.isAndroid) {
@@ -30,8 +29,7 @@ class Siren {
   /// This method will get the local version downloaded of your app.
   Future<Version> get storeVersion async {
     final packageInfo = await PackageInfo.fromPlatform();
-    _response =
-        await _getStoreClient().getStoreResponse(from: packageInfo.packageName);
+    _response = await _getStoreClient().getStoreResponse(from: packageInfo.packageName);
     return Version.parse(_response.version);
   }
 
@@ -48,6 +46,58 @@ class Siren {
     final storeVer = await storeVersion;
 
     return storeVer > localVer;
+  }
+
+  Future<void> promptUpdateV2(BuildContext context,
+      {String title = 'Update Available',
+      String message = '''
+There is an updated version available on the App Store. Would you like to upgrade?''',
+      String buttonUpgradeText = 'Upgrade',
+      String buttonCancelText = 'Cancel',
+      bool forceUpgrade = false}) async {
+    final isUpdateAvailable = await updateIsAvailable();
+    if (!isUpdateAvailable) {
+      return;
+    }
+    if (context.mounted) {
+      return showDialog<void>(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) {
+          final buttons = <Widget>[];
+
+          if (!forceUpgrade) {
+            buttons.add(TextButton(
+              child: Text(buttonCancelText),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ));
+          }
+
+          buttons.add(TextButton(
+            child: Text(buttonUpgradeText),
+            onPressed: () async {
+              final url = _response.url;
+
+              if (url != '' && await canLaunch(url)) {
+                await launch(url, forceSafariVC: false);
+              }
+
+              if (!forceUpgrade) {
+                Navigator.of(context).pop();
+              }
+            },
+          ));
+
+          return AlertDialog(
+            title: Text(title),
+            content: Text(message),
+            actions: buttons,
+          );
+        },
+      );
+    }
   }
 
   /// This method shows an customizable AlertDialog if an update is available.
